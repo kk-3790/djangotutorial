b8af307f1b6bc4014e9ae7be3a6f798b7ec0dbcb
kk-3790 <kkpatel3790@gmail.com>
2026-02-07 14:40:16 +0530

    This is Lab2 of Django

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 from django.shortcuts import redirect
 
 def home(request):
-    return redirect("polls/")
+    return redirect("admin/")
 
 urlpatterns = [
     path("", home),
